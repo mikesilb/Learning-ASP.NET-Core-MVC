10e0eec6c2856e3de2662a925d73729adfc918d0
Michael Silberstein <mikesilb123@gmail.com>
2018-01-14 15:38:34 -0500

    Altered Delete POST method within Controller

--- tutorial/MvcMovie/MvcMovie/Controllers/MoviesController.cs
+++ tutorial/MvcMovie/MvcMovie/Controllers/MoviesController.cs
@@ -162,9 +162,9 @@ namespace MvcMovie.Controllers
         }
 
         // POST: Movies/Delete/5
-        [HttpPost, ActionName("Delete")]
+        [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
+        public async Task<IActionResult> Delete(int id, bool notUsed)
         {
             var movie = await _context.Movie.SingleOrDefaultAsync(m => m.ID == id);
             _context.Movie.Remove(movie);
