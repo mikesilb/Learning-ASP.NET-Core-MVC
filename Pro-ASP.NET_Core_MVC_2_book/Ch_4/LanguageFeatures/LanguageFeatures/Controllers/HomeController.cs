18d2ef4b338ff8c5019f6fac3ed49da0e822b34d
Michael Silberstein <mikesilb123@gmail.com>
2018-01-25 15:59:26 -0500

    Ch4 Initial View

---
+++ Pro-ASP.NET_Core_MVC_2_book/Ch_4/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            return View(new string[] { "C#", "Language", "Features" });
+        }
+    }
+}
