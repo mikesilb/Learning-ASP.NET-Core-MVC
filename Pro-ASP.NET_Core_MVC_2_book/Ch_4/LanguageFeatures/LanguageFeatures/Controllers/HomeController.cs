f84b4714f71a12dd5311ca2d29ac0abedf0afb21
Michael Silberstein <mikesilb123@gmail.com>
2018-01-25 17:15:57 -0500

    Ch4 Using Automatically Implemented Properties

--- Pro-ASP.NET_Core_MVC_2_book/Ch_4/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
+++ Pro-ASP.NET_Core_MVC_2_book/Ch_4/LanguageFeatures/LanguageFeatures/Controllers/HomeController.cs
@@ -17,8 +17,10 @@ namespace LanguageFeatures.Controllers
                 string name = p?.Name ?? "<No Name>";
                 decimal? price = p?.Price ?? 0;
                 string relatedName = p?.Related?.Name ?? "<None>";
+                string category = p?.Category ?? "<None>";
+                bool? instock = p?.InStock ?? false;
 
-                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}", name, price, relatedName));
+                results.Add(string.Format("Name: {0}, Price: {1}, Related: {2}, Category: {3}, InStock: {4}", name, price, relatedName, category, instock));
             }
 
             return View(results);
