f8ead92ac97c8ce24b2b86afeb06baf992763d36
Michael Silberstein <mikesilb123@gmail.com>
2018-02-01 13:23:33 -0500

    Ch5 Prepping MVC Setup

---
+++ Pro-ASP.NET_Core_MVC_2_book/Ch_5/Razor/Razor/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Razor.Models;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Razor.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            Product myProduct = new Product
+            {
+                ProductID = 1,
+                Name = "Kayak",
+                Description = "A boat for 1 person",
+                Category = "Watersports",
+                Price = 275M
+            };
+            return View(myProduct);
+        }
+    }
+}
