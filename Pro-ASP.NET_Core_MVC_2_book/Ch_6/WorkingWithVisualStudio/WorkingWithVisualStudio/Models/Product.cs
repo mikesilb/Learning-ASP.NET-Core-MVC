1a5c764141eeeb2fa0f7e58f69d8cfc4cab7b553
Michael Silberstein <mikesilb123@gmail.com>
2018-02-07 13:15:11 -0500

    Ch6 Prep Model, Controller, and Index View

---
+++ Pro-ASP.NET_Core_MVC_2_book/Ch_6/WorkingWithVisualStudio/WorkingWithVisualStudio/Models/Product.cs
@@ -0,0 +1,9 @@
+﻿using System;
+namespace WorkingWithVisualStudio.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+    }
+}
