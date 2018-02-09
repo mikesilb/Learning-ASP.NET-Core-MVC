014526f5f9ee7e508052f77c0ed683f2f2b2d16f
Michael Silberstein <mikesilb123@gmail.com>
2018-02-09 12:19:36 -0500

    Ch6 Tied up loose ends using Visual Studio

--- Pro-ASP.NET_Core_MVC_2_book/Ch_6/WorkingWithVisualStudio/WorkingWithVisualStudio/Startup.cs
+++ Pro-ASP.NET_Core_MVC_2_book/Ch_6/WorkingWithVisualStudio/WorkingWithVisualStudio/Startup.cs
@@ -22,6 +22,8 @@ namespace WorkingWithVisualStudio
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
             app.UseDeveloperExceptionPage();
+            app.UseBrowserLink();
+            app.UseStaticFiles();
             app.UseMvcWithDefaultRoute();
         }
     }
