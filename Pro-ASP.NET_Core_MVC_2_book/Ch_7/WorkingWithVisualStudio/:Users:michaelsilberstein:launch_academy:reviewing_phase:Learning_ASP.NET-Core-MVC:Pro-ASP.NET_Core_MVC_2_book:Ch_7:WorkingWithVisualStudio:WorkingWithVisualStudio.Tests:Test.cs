d85de72379913175d2a44a66a6fa9e4e1701100c
Michael Silberstein <mikesilb123@gmail.com>
2018-02-15 18:25:48 -0500

    Ch 7 Testing for Product class

---
+++ Pro-ASP.NET_Core_MVC_2_book/Ch_7/WorkingWithVisualStudio/:Users:michaelsilberstein:launch_academy:reviewing_phase:Learning_ASP.NET-Core-MVC:Pro-ASP.NET_Core_MVC_2_book:Ch_7:WorkingWithVisualStudio:WorkingWithVisualStudio.Tests:Test.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using WorkingWithVisualStudio.Mo;
+namespace WorkingWithVisualStudio.Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+        }
+    }
+}
